695401f4538fc1a1d3647503f8be15a4ed9b194e
Andrew Chepey <achepey@credera.com>
2021-05-31 16:18:53 -0500

    updating cart controller

--- spring/shop/src/main/java/com/example/shop/cart/CartController.java
+++ spring/shop/src/main/java/com/example/shop/cart/CartController.java
@@ -2,7 +2,6 @@ package com.example.shop.cart;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CartController {
 
-    private final CartService cartService;
-
     @Autowired
-    public CartController(CartService cartService) {
-        this.cartService = cartService;
-    }
+    private CartService cartService;
 
     @GetMapping("/cart/{id}")
     public ResponseEntity<Cart> getCartById(@PathVariable(value = "id") long cartId) {
