b926d052db2cdc28f70bde858de6c05ef7a8b4ec
Isaac Hargett <Isaac-Hargett>
2023-06-08 13:45:52 -0500

    added bar and going to try to link data to a json for testing purposes/ failsafe if we cannot conect backend

---
+++ group-project-ui/src/modules/pages/home-page/HackerNewsStories.js
@@ -0,0 +1,15 @@
+const HackerNewsStories = ({stories = []}) => {
+  return (
+    <div className="stories-wrapper">
+      {stories &&
+        stories.map(({ objectID, url, title, author, points }) => (
+          title && url &&
+          <div className='stories-list' key={objectID}>
+            <h3><a href={url} target="_blank" rel="noreferrer">{title}</a> - By <b>{author}</b> ({points} points)</h3>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default HackerNewsStories;
---
+++ group-project-ui/src/modules/pages/home-page/home-page.component.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import HackerNewsStories from './HackerNewsStories';
+import SearchBar from './search-bar.component';
+
+
+export const HackerNewsStoriesWithSearch = () => {
+  const [stories, setStories] = useState([]);
+  const [allStories, setAllStories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [keyword, setKeyword] = useState('');
+
+  const fetchStories = async () => {
+    try {
+      const data = await (await fetch('https://hn.algolia.com/api/v1/search_by_date?tags=front_page&hitsPerPage=20')).json();
+      const stortedStories = data.hits.sort((story, nextStory) => (story.points < nextStory.points ? 1 : -1));
+      setAllStories(stortedStories);
+      setStories(stortedStories);
+      setError(null);
+    } catch (err) {
+      setError(err.message);
+      setStories(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updateKeyword = (keyword) => {
+    const filtered = allStories.filter(story => {
+     return `${story.title.toLowerCase()} ${story.author.toLowerCase()}`.includes(keyword.toLowerCase());
+    })
+    setKeyword(keyword);
+    setStories(filtered);
+ }
+
+  useEffect(() => {
+    fetchStories();
+  }, []);
+
+  return (
+    <> { /* React fragment */}
+    <div className="wrapper">
+      <h2>Sauces: </h2>
+      {loading && <div>Sauces loading...</div>}
+      {error && <div>{`Problem fetching Sauces - ${error}`}</div>}
+      <SearchBar keyword={keyword} onChange={updateKeyword}/>
+      <HackerNewsStories stories={stories} />
+    </div>
+    </>
+  )
+}
+
+export default HackerNewsStoriesWithSearch;
