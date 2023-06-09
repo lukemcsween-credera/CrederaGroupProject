76db663d1f0712b238ea1f326c972214606282fd
lukemcsween-credera <luke.mcsween@credera.com>
2023-06-09 00:30:41 -0500

    Display page ready to take parameters

--- group-project-ui/src/modules/pages/display/display.component.jsx
+++ group-project-ui/src/modules/pages/display/display.component.jsx
@@ -1,8 +1,24 @@
 import React, { useEffect } from 'react';
 
-export const DisplayComponent = () => {
+export const DisplayComponent = (props) => {
+
+    const title = "props.brand";
+    const img = "props.filepath";
+    const description = "props.description";
+    const price = "props.price";
+    const inStock = "props.number";
 
     return (
-        <div>Display Page</div>
+        <div>
+            Display Page
+            
+            <h1>{title}</h1>
+            <h1>{img}</h1>
+            <h1>About the sauce:{description}</h1>
+            <h1>{price}</h1>
+            <h1>Number in Stock: {inStock}</h1>
+            
+            </div>
+ 
     )
 }
